7250d27cc1f83ab09d62861c15f828af6a5f11e0
rivagtng <rivaramadhan776@gmail.com>
2025-09-11 04:25:16 -0700

    Update dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title', 'Dashboard') - BKK OPAT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/dashboardadmin.css') }}" />
 </head>
 <body>
     <!-- Navbar -->
@@ -41,6 +41,6 @@
         </main>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/dashboardadmin.js') }}"></script>
 </body>
 </html>
