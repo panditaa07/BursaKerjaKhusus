6d52a74083cf09f311e39dcf600d9197f89442ef
kaeylandri <keypengenturu@gmail.com>
2025-10-09 15:05:03 +0700

    malka, merapihkan company

--- resources/views/company/profile/show.blade.php
+++ resources/views/company/profile/show.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'Profil Perusahaan')
 
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/profilcompany.css') }}">
+@endpush
+
 @section('content')
 <div class="container">
     <h1>Profil Perusahaan</h1>
