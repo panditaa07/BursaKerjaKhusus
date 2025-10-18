3a719d7f5ea644e527abe9b97b920f61baa22c9f
MalkaGanteng <malka31ataya@gmail.com>
2025-10-18 19:28:10 +0700

    Malka - Konsistenkan warna

--- resources/views/company/jobs/create.blade.php
+++ resources/views/company/jobs/create.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.dashboard')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/tambahlowongancom.css') }}?v={{ time() }}">
+@endsection
+
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
@@ -164,19 +168,19 @@
                         </div>
 
                         <!-- Form Actions -->
-                        <div class="row">
-                            <div class="col-12">
-                                <hr class="my-4">
-                                <div class="d-flex justify-content-between">
-                                    <a href="{{ url()->previous() }}" class="btn btn-secondary">
-                                        <i class="fas fa-arrow-left me-2"></i>Kembali
-                                    </a>
-                                    <button type="submit" class="btn btn-primary btn-lg">
-                                        <i class="fas fa-save me-2"></i>Simpan Lowongan
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
+<div class="row">
+    <div class="col-12">
+        <hr class="my-4">
+        <div class="d-flex justify-content-between align-items-center gap-3">
+            <a href="{{ url()->previous() }}" class="btn btn-secondary flex-shrink-0">
+                <i class="fas fa-arrow-left me-2"></i>Kembali
+            </a>
+            <button type="submit" class="btn btn-primary flex-shrink-0">
+                <i class="fas fa-save me-2"></i>Simpan Lowongan
+            </button>
+        </div>
+    </div>
+</div>
                     </form>
                 </div>
             </div>
